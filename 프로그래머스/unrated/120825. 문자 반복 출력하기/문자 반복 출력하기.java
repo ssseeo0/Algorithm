1ef791875e5c1e1b679287fd1384f0efb8b03df6
seo02wow <135966211+seo02wow@users.noreply.github.com>
2024-01-06 20:04:12 +0900

    Update 문자 반복 출력하기.java

--- 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -11,3 +11,13 @@ class Solution {
         return answer.toString();
     }
 }
+// 다른 사람의 풀이
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : my_string.toCharArray()){
+            sb.append((c + "").repeat(n));
+        }
+        return sb.toString();
+    }
+}
