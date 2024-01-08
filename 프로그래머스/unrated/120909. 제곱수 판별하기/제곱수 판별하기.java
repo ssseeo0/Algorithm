6221ea10ae7cf2a25a4aa638ef5a508ccd592cc2
seo02wow <135966211+seo02wow@users.noreply.github.com>
2024-01-08 23:59:05 +0900

    Update 제곱수 판별하기.java

--- 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -10,3 +10,14 @@ class Solution {
         return answer;
     }
 }
+// Math.sqrt(n) : n의 제곱근 구하는 메소드 
+// 다른 사람의 풀이 
+class Solution {
+    public int solution(int n) {
+        if (n % Math.sqrt(n) == 0) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
