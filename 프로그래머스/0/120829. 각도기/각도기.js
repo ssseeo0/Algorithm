5a770f6e3f51ce5be7277073c9351dc9f06e3fb2
seo02wow <135966211+seo02wow@users.noreply.github.com>
2024-03-03 12:16:56 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,13 +1,3 @@
 function solution(angle) {
-    var answer = 0;
-    if (angle > 0 && angle < 90){
-        answer = 1;
-    } else if(angle > 90 && angle < 180){
-        answer = 3;
-    } else if(angle === 90){
-        answer = 2;
-    } else if(angle === 180){
-        answer = 4;
-    }
-    return answer;
+    return angle < 90 ? 1 : angle === 180 ? 4 : angle > 90 ? 3 : 2 ;
 }
