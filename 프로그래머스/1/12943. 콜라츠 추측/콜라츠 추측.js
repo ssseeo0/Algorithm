2bcc98e8ae0f588281a7c4a82e7388789e6f66b3
seo02wow <135966211+seo02wow@users.noreply.github.com>
2024-03-13 23:54:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    var answer = 0;
+    
+    
+    while(num !== 1){
+        answer += 1;
+        if(num % 2 === 0){
+            num /= 2;
+        } else {
+            num = (num * 3) + 1;
+        }
+    }
+    
+    return answer > 500 ? -1 : answer;
+}
