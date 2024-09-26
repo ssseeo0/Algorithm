3cf738e059a40ff5ab6c958cf498a557f3c04926
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-09-26 14:11:59 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,9 @@
 function solution(array, commands) {
     var answer = [];
     commands.forEach(command => {
-        let i = command[0] - 1;
-        let j = command[1];
-        let k = command[2] - 1;
-        let slicedArray = array.slice(i, j).sort((a, b) => a - b);
-        answer.push(slicedArray[k]);
+        const [i, j, k] = command
+        let slicedArray = array.slice(i - 1, j).sort((a, b) => a - b);
+        answer.push(slicedArray[k - 1]);
       }
     )
     return answer;
